18d5170385820983b5b3a6258594aeb8f8e6f897
전해은 <ggoo7731@naver.com>
2025-03-06 15:21:13 +0900

    데이터베이스 생성 및 연결

--- diary_project/config/settings/base.py
+++ diary_project/config/settings/base.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DIARY_PROJECT',
+        'USER': 'law',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
